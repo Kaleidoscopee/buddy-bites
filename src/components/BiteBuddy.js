a4f46ba747e62cca6622aac8b36ffb31d46e18f0
Kaleidoscopee <133397937+Kaleidoscopee@users.noreply.github.com>
2023-08-14 09:23:16 -0400

    jsx element plan

--- src/components/BiteBuddy.js
+++ src/components/BiteBuddy.js
@@ -3,8 +3,25 @@ import React from 'react'
 function BiteBuddy() {
   return (
     <div>
+      <div>
       <h1>BiteBuddy</h1>
+      <p>Search Bar (put to right of logo)</p>
+      </div>
+
+      <div>image from api (center)</div>
+
+      <div>
+        <div>
+          <li>calories - (fill in val)</li>
+          <li>protein - (fill in val)</li>
+          <li>carbs - (fill in val)</li>
+          <li>fats - (fill in val)</li>
+        </div>
+      
+      </div>
+      
     </div>
+
   )
 }
 
