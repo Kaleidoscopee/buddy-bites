8bb98e0003294808f1dc23e4d8b1a4392f64016e
Kaleidoscopee <133397937+Kaleidoscopee@users.noreply.github.com>
2023-08-14 08:51:05 -0400

    CSS attempt not printing though

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import BiteBuddy from "./components/BiteBuddy";
+import "./index.css" // used https://www.w3schools.com/react/react_css.asp as a refrence to start my css
 
 function App() {
   return (
     <div className="App">
-      <BiteBuddy />
+      <BiteBuddy className="biteBuddy"/>
+
     </div>
   );
 }
